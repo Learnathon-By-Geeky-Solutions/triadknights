53ba7ac8e9667632324a99be60ebd394164c1f7f
Munawer-Taj <taj4580@gmail.com>
2025-02-04 18:45:07 +0600

    Use of ModelAndView instead of Model is done

--- Personal Contribution/Munawer/MyCalculator/src/main/java/com/SpringBootWeb/MyCalculator/HomeController.java
+++ Personal Contribution/Munawer/MyCalculator/src/main/java/com/SpringBootWeb/MyCalculator/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -16,12 +17,13 @@ public class HomeController {
     }
 
     @RequestMapping("/add")
-    public String add(int num1, int num2, Model model) {
+    public ModelAndView add(int num1, int num2, ModelAndView mv) {
         int result = num1 + num2;
 
-        model.addAttribute("result", result);
+        mv.addObject("result", result);
+        mv.setViewName("result");
 
-        return "result";
+        return mv;
     }
 
 }
