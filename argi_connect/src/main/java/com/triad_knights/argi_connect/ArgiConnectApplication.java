4cad75ae6a0089441fdb27bc0888554e94bf7411
Mamun Miah <mamunmiah193gsc@gmail.com>
2025-02-27 19:36:44 +0600

    Initail application is added

---
+++ argi_connect/src/main/java/com/triad_knights/argi_connect/ArgiConnectApplication.java
@@ -0,0 +1,13 @@
+package com.triad_knights.argi_connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgiConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArgiConnectApplication.class, args);
+	}
+
+}
---
+++ argi_connect/src/test/java/com/triad_knights/argi_connect/ArgiConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.triad_knights.argi_connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgiConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
