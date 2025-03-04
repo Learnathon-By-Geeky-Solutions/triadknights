cd79517f00d20b77bb3a14a2b783a9b560059b72
Mamun Miah <mamunmiah193gsc@gmail.com>
2025-03-04 10:13:12 +0600

    Intial project refactored

--- agri_connect/src/main/java/com/triad_knights/agri_connect/AgriConnectApplication.java
+++ agri_connect/src/main/java/com/triad_knights/agri_connect/AgriConnectApplication.java
@@ -1,13 +1,13 @@
-package com.triad_knights.argi_connect;
+package com.triad_knights.agri_connect;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ArgiConnectApplication {
+public class AgriConnectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ArgiConnectApplication.class, args);
+		SpringApplication.run(AgriConnectApplication.class, args);
 	}
 
 }
